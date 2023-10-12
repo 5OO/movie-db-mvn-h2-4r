25186919272235ab7b999d0d5721e1805197192d
5OO <git.hub@myrk.eu>
2023-10-12 21:27:41 +0300

    test run the game

--- src/main/java/com/jdb/moviedbmvnh24r/repository/MovieRepository.java
+++ src/main/java/com/jdb/moviedbmvnh24r/repository/MovieRepository.java
@@ -4,5 +4,13 @@ package com.jdb.moviedbmvnh24r.repository;
 import com.jdb.moviedbmvnh24r.model.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 public interface MovieRepository extends JpaRepository<Movie, Long> {
+
+
+    List<Movie> findMovieByVoteAverageIsGreaterThan(BigDecimal voteAverage);
+
+
 }
