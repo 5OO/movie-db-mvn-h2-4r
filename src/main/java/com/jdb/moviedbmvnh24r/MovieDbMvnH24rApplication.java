93f15ecc7f70413b18b16ccc0a1832d94d8b8810
5OO <git.hub@myrk.eu>
2023-11-02 10:29:45 +0200

    removing Bean and CommandLineRunner from main application.

--- src/main/java/com/jdb/moviedbmvnh24r/MovieDbMvnH24rApplication.java
+++ src/main/java/com/jdb/moviedbmvnh24r/MovieDbMvnH24rApplication.java
@@ -1,12 +1,9 @@
 package com.jdb.moviedbmvnh24r;
 
-import com.jdb.moviedbmvnh24r.repository.MovieRepository;
 import com.jdb.moviedbmvnh24r.service.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
@@ -14,14 +11,9 @@ public class MovieDbMvnH24rApplication {
 
     @Autowired
     private GameService gameService;
+
     public static void main(String[] args) {
         SpringApplication.run(MovieDbMvnH24rApplication.class, args);
-    }
-
-    @Bean
-    public CommandLineRunner runner(MovieRepository movieRepository) {
-        return (args ->
-                gameService.playGame());
     }
 
 
